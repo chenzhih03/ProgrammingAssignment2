df0e0f777e756a72f2ead9e093e90f552de48830
Zion Chen <chenzhihong03@hotmail.com>
2016-10-09 22:01:45 -0400

    MY commit for cashe matrix

--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,32 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+    m<- NULL
+    set<-function(y){
+        x<<-y
+	m<<-NULL
+    }
+    get<-function() x
+    settheinverse<-function(solve) m<<-solve
+    gettheinverse<-function() m
+    list(set=set,get=get,settheinverse=settheinverse,gettheinverse=gettheinverse)
+
+
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m<-x$gettheinverse()
+    if(!is.null(m)){
+        message("getting cashed data")
+	return(m)
+    }
+    data<-x$get()
+    m<-solve(data,...)
+    x$settheinverse(m)
+    m
+
+## Return a matrix that is the inverse of 'x'
 }
